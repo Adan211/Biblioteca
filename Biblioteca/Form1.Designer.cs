7b3f4c6038e877179aaf19e3007f6ce6987548c4
Adan <villanuevaperez@gmail.com>
2023-07-18 08:26:06 -0600

    Agregar archivos de proyecto.

---
+++ Biblioteca/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace Biblioteca
+{
+    partial class CUBICULO_INDIVIDUAL
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cubiculo1 = new System.Windows.Forms.PictureBox();
+            this.Cubiculo3 = new System.Windows.Forms.PictureBox();
+            this.Cubiculo4 = new System.Windows.Forms.PictureBox();
+            this.Cubiculo5 = new System.Windows.Forms.PictureBox();
+            this.Cubiculo6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtId_Usuario = new System.Windows.Forms.TextBox();
+            this.rdbLiberar = new System.Windows.Forms.RadioButton();
+            this.rdbRegistrar = new System.Windows.Forms.RadioButton();
+            this.btnLiberar = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.grpbRegistrar = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCubiculo = new System.Windows.Forms.TextBox();
+            this.Cubiculo2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo6)).BeginInit();
+            this.grpbRegistrar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cubiculo1
+            // 
+            this.Cubiculo1.BackColor = System.Drawing.Color.Green;
+            this.Cubiculo1.Location = new System.Drawing.Point(26, 75);
+            this.Cubiculo1.Name = "Cubiculo1";
+            this.Cubiculo1.Size = new System.Drawing.Size(149, 73);
+            this.Cubiculo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cubiculo1.TabIndex = 0;
+            this.Cubiculo1.TabStop = false;
+            this.Cubiculo1.Click += new System.EventHandler(this.Cubiculo1_Click);
+            // 
+            // Cubiculo3
+            // 
+            this.Cubiculo3.BackColor = System.Drawing.Color.Green;
+            this.Cubiculo3.Location = new System.Drawing.Point(26, 353);
+            this.Cubiculo3.Name = "Cubiculo3";
+            this.Cubiculo3.Size = new System.Drawing.Size(149, 73);
+            this.Cubiculo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cubiculo3.TabIndex = 2;
+            this.Cubiculo3.TabStop = false;
+            this.Cubiculo3.Click += new System.EventHandler(this.Cubiculo3_Click);
+            // 
+            // Cubiculo4
+            // 
+            this.Cubiculo4.BackColor = System.Drawing.Color.Green;
+            this.Cubiculo4.Location = new System.Drawing.Point(209, 75);
+            this.Cubiculo4.Name = "Cubiculo4";
+            this.Cubiculo4.Size = new System.Drawing.Size(149, 73);
+            this.Cubiculo4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cubiculo4.TabIndex = 3;
+            this.Cubiculo4.TabStop = false;
+            this.Cubiculo4.Click += new System.EventHandler(this.Cubiculo4_Click);
+            // 
+            // Cubiculo5
+            // 
+            this.Cubiculo5.BackColor = System.Drawing.Color.Green;
+            this.Cubiculo5.Location = new System.Drawing.Point(209, 212);
+            this.Cubiculo5.Name = "Cubiculo5";
+            this.Cubiculo5.Size = new System.Drawing.Size(149, 73);
+            this.Cubiculo5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cubiculo5.TabIndex = 4;
+            this.Cubiculo5.TabStop = false;
+            this.Cubiculo5.Click += new System.EventHandler(this.Cubiculo5_Click);
+            // 
+            // Cubiculo6
+            // 
+            this.Cubiculo6.BackColor = System.Drawing.Color.Green;
+            this.Cubiculo6.Location = new System.Drawing.Point(209, 353);
+            this.Cubiculo6.Name = "Cubiculo6";
+            this.Cubiculo6.Size = new System.Drawing.Size(149, 73);
+            this.Cubiculo6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cubiculo6.TabIndex = 5;
+            this.Cubiculo6.TabStop = false;
+            this.Cubiculo6.Click += new System.EventHandler(this.Cubiculo6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CUBICULO 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CUBICULO 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "CUBICULO 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "CUBICULO 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(253, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "CUBICULO 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(253, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "CUBICULO 6";
+            // 
+            // txtId_Usuario
+            // 
+            this.txtId_Usuario.Location = new System.Drawing.Point(79, 68);
+            this.txtId_Usuario.Name = "txtId_Usuario";
+            this.txtId_Usuario.Size = new System.Drawing.Size(126, 20);
+            this.txtId_Usuario.TabIndex = 12;
+            // 
+            // rdbLiberar
+            // 
+            this.rdbLiberar.AutoSize = true;
+            this.rdbLiberar.Location = new System.Drawing.Point(473, 93);
+            this.rdbLiberar.Name = "rdbLiberar";
+            this.rdbLiberar.Size = new System.Drawing.Size(57, 17);
+            this.rdbLiberar.TabIndex = 13;
+            this.rdbLiberar.TabStop = true;
+            this.rdbLiberar.Text = "Liberar";
+            this.rdbLiberar.UseVisualStyleBackColor = true;
+            this.rdbLiberar.CheckedChanged += new System.EventHandler(this.rdbLiberar_CheckedChanged);
+            // 
+            // rdbRegistrar
+            // 
+            this.rdbRegistrar.AutoSize = true;
+            this.rdbRegistrar.Location = new System.Drawing.Point(473, 166);
+            this.rdbRegistrar.Name = "rdbRegistrar";
+            this.rdbRegistrar.Size = new System.Drawing.Size(67, 17);
+            this.rdbRegistrar.TabIndex = 14;
+            this.rdbRegistrar.TabStop = true;
+            this.rdbRegistrar.Text = "Registrar";
+            this.rdbRegistrar.UseVisualStyleBackColor = true;
+            this.rdbRegistrar.CheckedChanged += new System.EventHandler(this.rdbRegistrar_CheckedChanged);
+            // 
+            // btnLiberar
+            // 
+            this.btnLiberar.Location = new System.Drawing.Point(564, 87);
+            this.btnLiberar.Name = "btnLiberar";
+            this.btnLiberar.Size = new System.Drawing.Size(90, 29);
+            this.btnLiberar.TabIndex = 15;
+            this.btnLiberar.Text = "Liberar cubiculo";
+            this.btnLiberar.UseVisualStyleBackColor = true;
+            this.btnLiberar.Click += new System.EventHandler(this.btnLiberar_Click);
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(267, 71);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(90, 29);
+            this.btnRegistro.TabIndex = 16;
+            this.btnRegistro.Text = "Registrar";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // grpbRegistrar
+            // 
+            this.grpbRegistrar.Controls.Add(this.label8);
+            this.grpbRegistrar.Controls.Add(this.label7);
+            this.grpbRegistrar.Controls.Add(this.txtCubiculo);
+            this.grpbRegistrar.Controls.Add(this.btnRegistro);
+            this.grpbRegistrar.Controls.Add(this.txtId_Usuario);
+            this.grpbRegistrar.Location = new System.Drawing.Point(394, 312);
+            this.grpbRegistrar.Name = "grpbRegistrar";
+            this.grpbRegistrar.Size = new System.Drawing.Size(388, 119);
+            this.grpbRegistrar.TabIndex = 17;
+            this.grpbRegistrar.TabStop = false;
+            this.grpbRegistrar.Text = "Registar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Usuario";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Cubiculo";
+            // 
+            // txtCubiculo
+            // 
+            this.txtCubiculo.Location = new System.Drawing.Point(79, 27);
+            this.txtCubiculo.Name = "txtCubiculo";
+            this.txtCubiculo.Size = new System.Drawing.Size(126, 20);
+            this.txtCubiculo.TabIndex = 17;
+            // 
+            // Cubiculo2
+            // 
+            this.Cubiculo2.BackColor = System.Drawing.Color.Green;
+            this.Cubiculo2.Location = new System.Drawing.Point(26, 212);
+            this.Cubiculo2.Name = "Cubiculo2";
+            this.Cubiculo2.Size = new System.Drawing.Size(149, 73);
+            this.Cubiculo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cubiculo2.TabIndex = 18;
+            this.Cubiculo2.TabStop = false;
+            this.Cubiculo2.Click += new System.EventHandler(this.Cubiculo2_Click);
+            // 
+            // CUBICULO_INDIVIDUAL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 443);
+            this.Controls.Add(this.Cubiculo2);
+            this.Controls.Add(this.rdbRegistrar);
+            this.Controls.Add(this.btnLiberar);
+            this.Controls.Add(this.rdbLiberar);
+            this.Controls.Add(this.grpbRegistrar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cubiculo6);
+            this.Controls.Add(this.Cubiculo5);
+            this.Controls.Add(this.Cubiculo4);
+            this.Controls.Add(this.Cubiculo3);
+            this.Controls.Add(this.Cubiculo1);
+            this.Name = "CUBICULO_INDIVIDUAL";
+            this.Text = "Cubiculos individuales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo6)).EndInit();
+            this.grpbRegistrar.ResumeLayout(false);
+            this.grpbRegistrar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Cubiculo2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Cubiculo1;
+        private System.Windows.Forms.PictureBox Cubiculo3;
+        private System.Windows.Forms.PictureBox Cubiculo4;
+        private System.Windows.Forms.PictureBox Cubiculo5;
+        private System.Windows.Forms.PictureBox Cubiculo6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtId_Usuario;
+        private System.Windows.Forms.RadioButton rdbLiberar;
+        private System.Windows.Forms.RadioButton rdbRegistrar;
+        private System.Windows.Forms.Button btnLiberar;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.GroupBox grpbRegistrar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCubiculo;
+        private System.Windows.Forms.PictureBox Cubiculo2;
+    }
+}
+
